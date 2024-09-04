d31ad7b9699947e05238d5cf24f092fd62233bde
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-09-04 16:32:10 -0700

    tokenizing butter but \n is a problem

--- scripts/parser/parser.c
+++ scripts/parser/parser.c
@@ -7,6 +7,14 @@
 
 vector tokens;
 
+int is_valid_char(char c) {
+    if(c < 48 || c > 122) return 0;
+    if(c >= 58 && c <= 64) return 0;
+    if(c >= 91 && c <= 94) return 0;
+    if(c == 96) return 0;
+    return 1;
+}
+
 void print_tokens() {
     for(int i = 0; i < tokens.size; i++) {
         printf("token %d: %s\n", i, (char *)tokens.arr[i]);
@@ -24,10 +32,13 @@ int main(int argc, char** argv) {
     int i = 0;
     int nbytes = 1;
     while((nbytes = fread(buff + i, sizeof(char), 1, fp) > 0)) {
-        if(buff[i] == ' ') {
-            char* allocator = MY_ALLOC(strnlen(buff, MAXLEN) + 1); // excludes '\0'
-            strncpy(allocator, buff, MAXLEN); 
-            vector_add(&tokens, allocator);
+        if(buff[i] == ' ' || buff[i] == '\n') {
+            if(is_valid_char(buff[0]))  {
+                char* allocator = MY_ALLOC(strnlen(buff, MAXLEN) + 1); // excludes '\0'
+                strncpy(allocator, buff, MAXLEN); 
+                vector_add(&tokens, allocator);
+            }
+
             memset(buff, 0, MAXLEN);
             i = -1;
         }
